1a4e759cfe0d88cffdefea877c4f71aab18921db
Dave Millican <dave@skillerwhale.com>
2025-03-24 03:46:20 +0000

    Add exercises for TDD in Python (#1)

---
+++ exercises/introduction_to_tdd/src/wordle.py
@@ -0,0 +1,22 @@
+"""
+example Python function (in case you're not familiar with Python):
+-----------------------
+
+def name_of_the_function(arg1, arg2, arg3):
+
+    list_1 = ["a", "b", "c", "d", "e"]
+    list_2 = list("abcde")
+
+    # list_1 and list_2 are equal
+
+    for i, letter in enumerate("hello"):
+        list_2[i] = letter
+    # list_2 is now equal to ["h", "e", "l", "l", "o"]
+
+    string_1 = "".join(list_1)  # string_1 equals "abcde"
+
+    "a" in string_1  # true
+    "x" in string_1  # false
+
+    return 42  # value to return from the function
+"""
